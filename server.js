c46f5ed23431bc54affab1ce22068d1284010a2e
Jemhop <emma.jellemabutler@gmail.com>
2021-04-17 23:53:12 +0200

    Some tag matching code I'm really not proud of that I also haven't tested yetr

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-
 let searchingUsers = []
 let onlineUsers = 0;
 
@@ -26,13 +25,11 @@ io.on('connection', socket =>  {
     socket.emit('ID', socket.id);
 
     socket.on('tagSubmit', (user = {userID, tags}) => {
-        if(searchingUsers.length < 2) 
+        if(searchingUsers.length >= 2) 
         {
             recursiveMatch();
         }
         searchingUsers.push(user);
-        console.log(user.tags);
-        console.log(user.userID);
 
         for(let i = 0; i <searchingUsers.length; i++)
         {
@@ -57,15 +54,46 @@ io.on('connection', socket =>  {
     })
 });
 
-function recursiveMatch()
+
+//this code is so bad, like genuinely, really, actually, genuinely so bad
+//please, please, please, future me majorly rewrite it
+//its also SO fucking slow (i believe it's O(N) for a single user and O(N^2) for the whole list of searching users? idk)
+function matchingManager()
 {
-    currentUserIndex;
+    //this is badbadbadbadbadbadbadbadbadbadbadbad but i wanted to keep business logic outside of my matching function
+    let currentUserIndex = 0;
+    currentUser = searchingUsers[currentUserIndex]
+    matchResult = recursiveMatch(currentUserIndex)
+    if(matchResult.isArray())
+    {
+        return matchResult;
+    }
+    else if(!matchResult.isArray() && searchingUsers.length >= 2)
+    {
+        recursiveMatch(currentUserIndex)
+    }
+
+}
+
+function recursiveMatch(currentUserIndex)
+{
+    //this bit is decent
     currentUser = searchingUsers[0].id;
     userTags = searchingUsers[0].tags
-    if(searchingUsers.length > 1)
+    for(let i = 0; i < searchingUsers.length; i++)
     {
-        recursiveMatch()
+        let sharedTags = _.intersection(userTags, searchingUsers[i].tags)
+        //intersection returns an array of all shared items
+        if(sharedTags.length > 0)
+        {
+            //remove and return if sucessful
+            searchingUsers.splice(currentUser, 1);
+            return sharedTags;
+        }
     }
+    //else move onto the next user in the array
+    return currentUserIndex
+
 }
 
 
